4a518f6a1d1fff9539e305e4f8052b1f7b4b9af9
florfu <97113940+florfu@users.noreply.github.com>
2022-06-15 20:16:41 -0300

    Minor changes in code comments

--- game.js
+++ game.js
@@ -1,17 +1,9 @@
-//var where the game pattern is gonna be stored
-var gamePattern = [];
 
-//game colours
-var buttonColours = ["red", "blue", "green", "yellow"];
-
-//var where the button's clicked id is being stored
-var userClickedPattern = [];
-
-//var that tracks keypresses
-var started = false;
-
-//var that tracks the game level
-var level = 0;
+var gamePattern = []; // array where the game pattern is gonna be stored
+var buttonColours = ["red", "blue", "green", "yellow"]; // game colours
+var userClickedPattern = []; // array where button's clicked id is being stored
+var started = false; // boolean that tracks if game is started
+var level = 0; // tracks levels
 
 //Start the game when a key is pressed
 $(document).keydown(function() {
@@ -24,37 +16,31 @@ $(document).keydown(function() {
 
 //handler function for users' button clicks
 $(".btn").click(function() {
-  //get id (colour) from button clicked
-  var userChosenColour = this.id;
-  //animate button clicked
-  animatePress(userChosenColour);
-  //play sound of button clicked by user
-  playSound(userChosenColour);
-  //store colour in userClickedPattern array
-  userClickedPattern.push(userChosenColour);
-  //show index of last array element
-  lastClickedIndex = userClickedPattern.length -1;
-  checkAnswer(lastClickedIndex);
+  var userChosenColour = this.id; //get id (colour) from button clicked
+ 
+  animatePress(userChosenColour); //animate button clicked
+  playSound(userChosenColour); //play sound of button clicked by user
+  
+  userClickedPattern.push(userChosenColour); //store clicked colour in userClickedPattern array
+ 
+  lastClickedIndex = userClickedPattern.length -1; //show index of last array element
+  checkAnswer(lastClickedIndex); // calls function that checks if answer is correct
 });
 
 
 // this function runs when user finishes his turn
 // selects a random colour from buttonColours and adds it at the end of gamePattern
 function nextSequence() {
-  //reset userClickedPattern
-  userClickedPattern=[];
-  //update level and title
-  level++;
-  $("#level-title").text("Level "+level);
+  userClickedPattern=[]; // reset userClickedPattern
+  level++; // update level
+  $("#level-title").text("Level "+level); // update title
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
-  //adds randomChosenColour to the array gamePattern
-  gamePattern.push(randomChosenColour);
-  //button flash animation
-  $("." + randomChosenColour).fadeOut(200).fadeIn(200);
-  //play the sound for the button colour
-  playSound(randomChosenColour);
+  gamePattern.push(randomChosenColour);// adds randomChosenColour to the array gamePattern
+
+  $("." + randomChosenColour).fadeOut(200).fadeIn(200); // button flash animation
+  playSound(randomChosenColour); //play the sound for the button colour
 }
 
 // function to start over game after user misses button
@@ -66,20 +52,19 @@ function startOver() {
 
 //Check answer function
 function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel]===userClickedPattern[currentLevel]) {
-    if (userClickedPattern.length === gamePattern.length) {
+  if (gamePattern[currentLevel]===userClickedPattern[currentLevel]) { // compares colur clicked with same index colour in gamePattern
+    if (userClickedPattern.length === gamePattern.length) { // if answer is right & it's the last element of the array
       setTimeout(() => nextSequence(),1000);
-    }
-    else {
-      console.log("not the last");
     }
   } else { // user fails
     playSound('wrong');
+    // background turns red animation
     $("body").addClass("red");
     setTimeout(function(){
       $("body").removeClass("red")
     },200);
-    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    $("#level-title").text("Game Over, Press Any Key to Restart"); // change title to Game Over
     startOver(); // resets game variables
   }
 }
